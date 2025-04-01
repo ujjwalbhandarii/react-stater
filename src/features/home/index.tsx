a314293e0ef5ebeca3bad30c73fca13f20c5d289
ujjwalbhandarii <ujjwalbhandari14@gmail.com>
2025-04-01 22:47:29 +0545

    feat: update to react19

--- src/features/home/index.tsx
+++ src/features/home/index.tsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
   return (
-    <h1 className='bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3'>
-      working🚀🚀🚀🚀🚀
-    </h1>
+    <div className='h-screen bg-black text-white'>
+      <h1 className='px-4 py-2 sm:px-8 sm:py-3'>working🚀🚀🚀🚀🚀</h1>
+    </div>
   );
 }
